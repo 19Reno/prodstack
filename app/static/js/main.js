46d8cfea1db6ad85bb22e3fe9339a5b606e1e151
19Reno <jeroon67@gmail.com>
2026-03-20 09:49:20 +0000

    feat: add portfolio website with HTML CSS JS

---
+++ app/static/js/main.js
@@ -0,0 +1,17 @@
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Message sent! Thank you.');
+  this.reset();
+});
+
+// Smooth scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+console.log('ProdStack website loaded');
